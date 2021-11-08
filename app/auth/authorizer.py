0fb7aeb982b9002f5b929931e099fb5b8afb7784
Jonas Ferreira <jonas.abreuferreira>
2021-11-08 08:46:46 -0300

    adding new docstrings

--- app/auth/authorizer.py
+++ app/auth/authorizer.py
@@ -13,15 +13,16 @@ class Authorizer:
     """Authorizer class that performs validations for each transaction"""
     def __init__(
         self,
-        events: Iterable,
+        events: Iterable[dict],
         validations: Iterable[BaseValidation]
     ):
         """
         Constructor method for Authorizer class
 
         Args:
-            events (Iterable):
-            validations (Iterable[BaseValidation]):
+            events (Iterable[dict]): An iterable object of dictionaries, where dicts are the events to be validated
+            validations (Iterable[BaseValidation]): An iterable object of 'BaseValidation',
+            that will be used to validate the transactions contained on 'events' parameter
         """
         self.events = events
         self.validations = validations
@@ -29,6 +30,7 @@ class Authorizer:
 
     def process(self) -> None:
         """
+        Starts the validations for all transactions.
 
         Returns:
             None
@@ -41,9 +43,6 @@ class Authorizer:
             process_method = getattr(self, f"process_{event_type}")
             response = process_method(event=event)
 
-            # Update event dict
-            event.update(response)
-
             # Write event to stdout
             sys.stdout.write(json.dumps(response, default=str, indent=4, sort_keys=True))
 
@@ -53,7 +52,7 @@ class Authorizer:
         If account already been created, return 'account-already-initialized' violation
 
         Args:
-            event (dict): Event with information related to account.
+            event (dict): Event with information related to account creation.
 
         Returns:
             event (dict): An dictionary with updated information related to account creation and possible violations
@@ -75,45 +74,64 @@ class Authorizer:
 
     def process_transaction(self, event: dict) -> dict:
         """
+        Process events related to account transactions. Creates a new key value pair at event containing,
+        if founded, any violations based on predefined validations.
 
         Args:
-            event:
+            event (dict): Event with information related to an account transaction
 
         Returns:
-
+            event (dict): The original event with possible founded violations.
+            If wasn't founded any violation, violations field will be an empty list.
         """
+        # Checks if account exists
         if not self.account:
-            return {'violations': ['account-not-initialized']}
+            event.update({'violations': ['account-not-initialized']})
 
         else:
             violations = self.apply_validations(transaction=event)
-            return {'violations': [v for v in violations]}
+            event.update({'violations': [v for v in violations]})
+
+        return event
 
     def process_unknown(self, event: dict) -> dict:
         """
+        Process unknown events. Just add 'unknown-error' to violation field
 
         Args:
-            event:
+            event (dict): Event that wasn't in any expected type, like 'account_creation' or 'transaction'
 
         Returns:
-
+            event (dict): The original event with violations field
         """
-        return {'violations': ['unknown-error']}
+        event.update({'violations': ['unknown-error']})
+
+        return event
 
     def apply_validations(self, transaction: dict) -> Generator[str]:
         """
+        Apply validations specified on constructor to a single transaction.
 
         Args:
-            transaction:
+            transaction (dict): An transaction of type 'transaction' to be validated
 
-        Returns:
-
+        Yield:
+            violation (dict): Violation founded on validator
         """
+        # Apply validations
         for validator in self.validations:
             violation = validator().validate(
                 account=self.account,
                 transaction=transaction
             )
 
+            # If violations was founded, returns yields the violation
             if violation:
                 yield violation
+
+            # When no violations was founded, register the transaction to account
+            else:
+                BankStatement.register(
+                    account=self.account,
+                    transaction=transaction
+                )
--- app/auth/validation/base_validation.py
+++ app/auth/validation/base_validation.py
@@ -4,6 +4,21 @@ from app.bank.account import BankAccount
 
 
 class BaseValidation(ABC):
+    """
+    An abstract class to represent the a transaction validator to be used on Authorizer.
+    To implement custom validations, just implement this class. The 'validate' method is the responsible for return the
+    final value of validation. Feel free to add others methods to the class.
+    """
     @abstractmethod
     def validate(self, account: BankAccount, transaction: dict) -> str:
+        """
+        Validate a single transaction based on rules defined here.
+
+        Args:
+            account: An 'BankAccount' object that represents an account bank where the transaction will be validated. Can't be None or empty.
+            transaction: A dict with the transaction to be validated
+
+        Returns:
+
+        """
         raise NotImplementedError
